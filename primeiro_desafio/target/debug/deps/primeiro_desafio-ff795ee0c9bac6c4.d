da83453edfddd0959a017a9691b627f460852f5f
Anna Lívia <145619570+souanna@users.noreply.github.com>
2025-04-07 19:02:28 -0300

    Delete primeiro_desafio directory

--- primeiro_desafio/target/debug/deps/primeiro_desafio-ff795ee0c9bac6c4.d
+++
@@ -1,5 +0,0 @@
-/workspaces/Anna.Silva.3NB/primeiro_desafio/target/debug/deps/primeiro_desafio-ff795ee0c9bac6c4: src/main.rs
-
-/workspaces/Anna.Silva.3NB/primeiro_desafio/target/debug/deps/primeiro_desafio-ff795ee0c9bac6c4.d: src/main.rs
-
-src/main.rs:
